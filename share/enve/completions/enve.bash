ca27f9ff7f6e84706251afd227b16a260798d187
ranlempow <ranlempow@users.noreply.github.com>
2023-05-15 00:10:23 +0800

    cleanup old files

--- share/enve/completions/enve.bash
+++ share/enve/completions/enve.bash
@@ -13,4 +13,251 @@ _sub() {
     fi
 }
 
-complete -F _sub sub
+newl="$(printf '\nx')"
+newl="${newl%x}"
+
+join_by() {
+    local IFS="$1"
+    shift
+    eval "${1}_STRING=\"\${${1}[*]}\""
+}
+
+split_to() {
+    local IFS="$1"
+    shift
+    # ARRAY=($ARRAY_STRING)
+    eval "${1}=(\$${1}_STRING)"
+}
+
+_comp_option() {
+    local cur="${COMP_WORDS[COMP_CWORD]}"
+    local LIST_STRING="$1"
+    local DUP_STRING="$2"
+    local LIST
+    local DUP
+    local word
+
+    split_to " $newl" DUP
+    join_by "$newl" DUP
+
+    split_to " $newl" LIST
+    join_by "$newl" LIST
+    LIST_STRING="$newl$LIST_STRING$newl"
+    for word in "${COMP_WORDS[@]}"; do
+        if [ "$word" != "$cur" ] && \
+           [ -z "${word%%-*}" ] && \
+           [ -n "${DUP_STRING%%*${newl}${word}${newl}*}" ] && \
+           [ -z "${LIST_STRING%%*${newl}${word}${newl}*}" ]; then
+            LIST_STRING="\
+${LIST_STRING%${newl}${word}${newl}*}\
+$newl\
+${LIST_STRING#*${newl}${word}${newl}}"
+        fi
+    done
+
+    COMPREPLY=($(compgen -W "$LIST_STRING" -- "$cur"))
+}
+
+true '
+
+anchors__opt_a2="a1 a2"
+anchors__opt_b0=""
+archors__args="arg1 [arg2]..."
+
+cmd --opt-a2 a1 a2 --opt-b0 arg1 [arg2]...
+
+anchors_eq__opt_a1="a1"
+cmd --opt-a1=a1
+
+
+'
+# anchors__$opt="SP1 [SP2]..."
+# anchors_eq__$opt="SP1 [SP2]..."
+# archors__args="SP1 [SP2]..."
+
+_count_arg_fixed() {
+    local arg
+    cnt=0
+    for arg in ${args[@]}; do
+        if [ -n "${arg##\[*}" ]; then
+            cnt=$((cnt + 1))
+        fi
+    done
+}
+
+_find_state_by_args_2() {
+    local kopt=$1
+    local pos=$2
+    local args
+
+    PASSIBLE_REST=
+    OPTARG=
+    # OPTOPTION=
+    if args=${!kopt} && [ -n "${args}" ]; then
+        args=($args)
+        local args_last=${args[${#args[@]}-1]}
+        local fixed_consume=0
+        while [ -n "${args[$i]}" ]; do
+            if [ -z "${args[$i]##\[*}" ]; then
+                break
+            fi
+            fixed_consume=$((fixed_consume+1))
+        done
+
+        if [ -n "${args[$pos]}" ]; then
+            OPTARG=${args[$pos]}
+            local i=0
+            while [ $i -lt $pos ]; do
+                if [ -z "${args[$i]##\[*}" ]; then
+                    PASSIBLE_REST="${PASSIBLE_REST:+$PASSIBLE_REST }$((i - fixed_consume))"
+                fi
+                i=$((i+1))
+            done
+        elif [ -z "${args_last%%*.}" ] || [ -z "${args_last%%*.\]}" ]; then
+            OPTARG=${args_last%%\]}
+            OPTARG=${args_last%%.}
+        else
+            PASSIBLE_REST=$((pos - fixed_consume))
+        fi
+        if [ -n "${OPTARG}" ] && [ -z "${OPTARG##\[*}" ]; then
+            OPTARG=${OPTARG%%\]}
+            OPTARG=${OPTARG##\[}
+            # OPTOPTION=1
+        fi
+        return
+    else
+        return 1
+    fi
+}
+
+# _find_state_by_args() {
+#     local kopt=$1
+#     local optpos=$2
+#     local args
+#     if args=${!kopt} && [ -n "${args}" ]; then
+#         local diff=$((COMP_CWORD - optpos))
+#         args=($args)
+#         local args_last=${args[${#args[@]}-1]}
+#         if [ -n "${args[$diff]}" ]; then
+#             OPTARG=${args[$diff]}
+#             CMDARG=
+#         elif [ -z "${args_last%%*.}" ] || [ -z "${args_last%%*.\]}" ]; then
+#             OPTARG=${args_last%%\]}
+#             OPTARG=${args_last%%.}
+#             CMDARG=
+#         else
+#             _count_arg_fixed
+#             OPTARG=
+#             CMDARG=$((COMP_CWORD - cnt - optpos))
+#         fi
+#         if [ -n "${OPTARG}" ] && [ -z "${OPTARG##\[*}" ]; then
+#             OPTARG=${OPTARG%%\]}
+#             OPTARG=${OPTARG##\[}
+#             CMDARG=1
+#         fi
+#         return
+#     else
+#         return 1
+#     fi
+# }
+
+_find_state_by_prev_anchor() {
+    local anchor="${COMP_WORDS[COMP_CWORD]}"
+    if [ -z "${anchor##-*}" ]
+        if  [ -z "${anchor%*=}" ]; then
+            opt=${anchor/-/_}
+            opt=${opt%=}
+            if ! _find_state_by_args "anchors_eq__${opt}" $COMP_CWORD; then
+                return 1
+            else
+                return 0
+            fi
+        else
+            return 0
+        fi
+    fi
+
+
+    local i=$((COMP_CWORD - 1))
+    while [ $i -ge 0 ]; do
+        anchor="${COMP_WORDS[$i]}"
+        if [ -z "${anchor##-*}" ]; then
+            if [ -z "${anchor%*=*}" ] || \
+               ! _find_state_by_args "anchors__${anchor/-/_}" $((COMP_CWORD - i)); then
+                PASSIBLE_REST=
+                OPTARG=
+                OPTOPTION=1
+            fi
+            break
+        fi
+        i=$((i - 1))
+    done
+
+    OPTARG_LIST=$OPTARG
+    # OPTARG2=
+    for rest in $PASSIBLE_REST; do
+        if _find_state_by_args "anchors__args" $((COMP_CWORD - $rest)); then
+            OPTARG_LIST="${OPTARG_LIST:+$OPTARG_LIST }$OPTARG"
+        fi
+    done
+
+    # if [ "$OPTOPTION" = "1" ]; then
+    #     OPTARG1=$OPTARG
+    #     if _find_state_by_args "anchors__args" $((COMP_CWORD - i)); then
+    #         OPTARG2=$OPTARG
+    #     fi
+    # fi
+
+    return 1
+}
+
+
+_bash_arguments() {
+    # example: _bash_arguments \
+    #            "--x,--xargs:"
+    for opt in "$@"; do
+
+    done
+    if _find_state_by_prev_anchor; then
+        if [ -n "$ARG" ] && command -v _comp_arg_$ARG >/dev/null; then
+            COMPREPLY=($(_comp_arg_$ARG))
+        fi
+        if [ -z "${NO_OPT}" ]; then
+            COMPREPLY+=($(_comp_option "$list" "$dups"))
+        fi
+    else
+        COMPREPLY=($(_comp_option "$list" "$dups"))
+    fi
+}
+
+
+# _comp_regex() {
+#     # if [[ "$TEST" =~ [^a-zA-Z0-9\ ] ]]; then BLAH; fi
+# }
+
+_enve() {
+    # echo "${newl}X$@$newl${COMP_WORDS[@]}$newl$COMP_CWORD$newl" >&2
+    local IFS=,
+    local last
+    echo "$COMP_CWORD,${COMP_WORDS[*]}," >&2
+    if [ $COMP_CWORD -gt 0 ]; then
+        last="$COMP_WORDS[$((COMP_CWORD - 1))]"
+    else
+        last=
+    fi
+
+    # list=$(seq 500 | sed -e 's/^/--/')
+    # _comp_option "$list"
+
+    # COMPREPLY=($(compgen -o filenames -X "*.enve.ini" -- "$cur"))
+    # COMPREPLY=($(compgen -d -G "*.enve.ini" -- "$cur"))
+    # COMPREPLY=($(compgen -P "$PWD$PWD-" -W "a b c" -- "$cur"))
+    # COMPREPLY=(aaa)
+
+}
+complete -F _enve enve
+
+# aa=(a b c "d e")
+# x=$(join_by ":" "${aa[@]}")
+# split_to_array "$x"
+# join_by ":" "${a[@]}"
--- share/enve/completions/enve.zsh
+++ share/enve/completions/enve.zsh
@@ -1,21 +1,150 @@
-# TODO: this is only a template
+#compdef enve
 
-if [[ ! -o interactive ]]; then
-    return
-fi
+autoload -U compinit
+compinit
 
-compctl -K _sub sub
-
-_sub() {
-    local word words completions
-    read -cA words
-    word="${words[2]}"
-
-    if [ "${#words}" -eq 2 ]; then
-        completions="$(sub commands)"
-    else
-        completions="$(sub completions "${word}")"
-    fi
-
-    reply=("${(ps:\n:)completions}")
+function _enve() {
+    _arguments '-f[profile]:set profile:()' '*'{-c,--config}'[config]:set config:->setconfig' \
+        - normal-install \
+        '--cc=-[attempt to compile using compiler]:compiler:->cache' \
+        ':command-x:((xxx\:x yyy\:y))' \
+        ':command-y:(xxx zzz)' \
+        - xx-group2 \
+        '-d:xx:(aa a2)' \
+        '-b:xx:(bb b2)'
+    # _message -r "Completing..."
 }
+
+
+# completions remain in cache until any tap has new commits
+__enve_completion_caching_policy() {
+    local -a tmp
+
+    # invalidate if cache file is missing or >=2 weeks old
+    tmp=( $1(mw-2N) )
+    (( $#tmp )) || return 0
+
+    # otherwise, invalidate if latest tap index file is missing or newer than
+    # cache file
+    tmp=( ${HOMEBREW_REPOSITORY:-/usr/local/Homebrew}/Library/Taps/*/*/.git/index(om[1]N) )
+    [[ -z $tmp || $tmp -nt $1 ]]
+}
+
+# _set_cache_policy() {
+#     # set default cache policy
+#     zstyle -s ":completion:${curcontext%:*}:*" cache-policy tmp
+#     [[ -n $tmp ]] ||
+#     zstyle ":completion:${curcontext%:*}:*" cache-policy \
+#     __enve_completion_caching_policy
+# }
+
+# _enve_fire() {
+#     :
+# }
+
+# enve [-D|-DD|-DDD] [-V|-VV|-VVV] [-Q|-QQ|-Q] [--debug=<options>]
+#      [--verbose-level=<LOGLEVEL>]
+#      [--logfile-level=<LOGLEVEL>]
+#      [--progress[=auto|=always|=never]]
+#      [--color[=auto|=always|=never]]
+#      [--logfile=*] [-v|--version] [-h|--help] [-I|--instant] <subcommand>
+function _enve() {
+    local curcontext="$curcontext" state state_descr line expl
+    local tmp ret=1
+
+    _arguments -C \
+        '(-V -VV -VVV -Q -QQ -QQQ --verbose-level)'{-D'[debug message]',-DD'[trace message]',-DDD'[output everything]'} \
+        '(-D -DD -DDD -Q -QQ -QQQ --verbose-level)'{-V'[verbose]',-VV'[more verbose]',-VVV'[very verbose]'} \
+        '(-D -DD -DDD -V -VV -VVV --verbose-level)'{-Q'[show warning and error]',-QQ'[show error only]',-QQQ'[output nothing]'} \
+        '(-D -DD -DDD -V -VV -VVV -Q -QQ -QQQ)--verbose-level=-[assign verbose level at output]:verbose level:->LEVEL' \
+        '--logfile-level=-[assign verbose level at logfile]:verbose level:->LEVEL' \
+        '--debug=-[trun on debug options]:debug options:->DEBUG' \
+        '--progress=-[display output at one line]:when to show progress:->WHEN' \
+        '--color=-[colorize the output]:when to show color:->WHEN' \
+        {-v,--version}'[display version information and exit]' \
+        {-h,--help}'[display help and exit]' \
+        '1:command:->command' \
+        '*::options:->options'
+
+    case "$state" in
+        LEVEL)
+            local -a levels
+            levels=(TRACE DEBUG INFO NOTICE WARNING ERROR CRITICAL)
+            _describe 'verbose levels' levels
+        ;;
+        DEBUG)
+            local -a debugopts
+            debugopts=(never timing cli)
+            _describe 'debug options' debugopts
+        ;;
+        WHEN)
+            local -a whens
+            whens=(auto always never)
+            _describe 'when to perform' whens
+        ;;
+        command)
+            local -a commands
+            eval "commands=($(enve commands --escape --split=":"))"
+            _describe -t common-commands 'common commands' commands
+            # __brew_commands && return 0
+        ;;
+        options)
+            local command
+            command="${line[1]}"
+            # change context to e.g. enve-list
+            curcontext="${curcontext%:*}-${command}:${curcontext##*:}"
+
+            # zstyle -s ":completion:${curcontext%:*}:*" cache-policy tmp
+            # [[ -n $tmp ]] ||
+            # zstyle ":completion:${curcontext%:*}:*" cache-policy \
+            # __enve_completion_caching_policy
+
+            # call completion for named command e.g. _enve_list
+            local completion_func="_enve_${command//-/_}"
+            _call_function ret "${completion_func}" # && return ret
+            [ "$ret" -eq 0 ] && return
+
+            if eval [ -z "\$${completion_func}_imported" ]; then
+                eval ${completion_func}_imported=1
+                if eval "$(enve completions zsh "$command")"; then
+                    _call_function ret "${completion_func}" # && return ret
+                    [ "$ret" -eq 0 ] && return
+                fi
+            fi
+            _message "a completion function is not defined for command: ${command}"
+            return 1
+        ;;
+    esac
+}
+
+
+
+# _enve_fire() {
+#     _arguments --
+# }
+# _enve() {
+#     _call_function ret _enve_fire
+# }
+
+compdef _enve enve
+
+
+# if [[ ! -o interactive ]]; then
+#     return
+# fi
+
+# compctl -K _sub sub
+
+# _sub() {
+#     local word words completions
+#     read -cA words
+#     word="${words[2]}"
+
+#     if [ "${#words}" -eq 2 ]; then
+#         completions="$(sub commands)"
+#     else
+#         completions="$(sub completions "${word}")"
+#     fi
+
+#     reply=("${(ps:\n:)completions}")
+# }
